2935b9e683047177c63785a670c6e43b562bd61c
jace <jace_lee2000@yahoo.com>
2024-10-14 19:28:25 +0800

    fixed - APIKEY and requirement.txt

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -1,10 +1,15 @@
 import os
+import st 
 from dotenv import load_dotenv
 from openai import OpenAI
 import tiktoken
 
 
-load_dotenv('.env')
+if load_dotenv('.env'):
+   #for local environment
+   OPENAI_KEY = os.getenv('OPENAI_API_KEY')
+else:
+   OPENAI_KEY = st.secrets['OPENAI_API_KEY']
 
 # Pass the API Key to the OpenAI Client
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
