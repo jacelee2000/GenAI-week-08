f33e6ab0ab68befbc9651a290f37beee7d53f04a
jace <jace_lee2000@yahoo.com>
2024-10-22 21:27:21 +0800

    put disclamer more prominent

--- pages/1_Bot_1.py
+++ pages/1_Bot_1.py
@@ -39,10 +39,10 @@ if form.form_submit_button("Submit"):
     df = pd.DataFrame(course_details)
     df 
 
-    st.write('''
-             IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+st.write('''
+            #### IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
 
-             Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+            #### Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
 
-             Always consult with qualified professionals for accurate and personalized advice.
-             ''')
+            #### Always consult with qualified professionals for accurate and personalized advice.
+            ''')
