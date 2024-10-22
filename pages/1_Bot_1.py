aaed1705d83ae8b7ceb7c4d4932a3b2ce25d5958
jace <jace_lee2000@yahoo.com>
2024-10-22 21:28:28 +0800

    disclamer

--- pages/1_Bot_1.py
+++ pages/1_Bot_1.py
@@ -39,7 +39,7 @@ if form.form_submit_button("Submit"):
     df = pd.DataFrame(course_details)
     df 
 
-st.write('''
+    st.write('''
             #### IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
 
             #### Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
